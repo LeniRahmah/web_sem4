9f5ca91a9a4bb8592c183a13a24bdcad3f7359f1
Leni Rahmah <lenirahmah051004@gmail.com>
2025-03-09 22:43:45 -0700

    membuat prodi

--- index.php
+++ index.php
@@ -3,9 +3,31 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>SIMPADU POLIBAN</title>
 </head>
+
 <body>
     <h1>DATA MAHASISWA</h1
+    <br>
+    <table border="1" cellspacing="0" cellspadding="5">
+        <head>
+            <th>No</th>
+            <th>NIM</th>
+            <th>Nama</th>
+    </head>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td>E020323032</td>
+            <td>Leni</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>E020323008</td>
+            <td>Fadia</td>
+        </tr>
+    </tbody>
+</table>
+
 </body>
 </html>
